aa8247c63ecdf139ce3e3c434a390e264162092f
Rayncor <sowa332@gmail.com>
2022-07-06 21:43:54 -0700

    fixed some CSS, figured out loops to display weather info in panels, added loop to correctly display days

--- script.js
+++ script.js
@@ -20,43 +20,77 @@ setInterval(() => {
 
 }, 1000);
 //this is my api key for open weather; not sure if this remains good forever. also not sure whether there is some reason to not make an API key a const but it seems like a const would be the right choice. 
-const APIKey = "2104664de283a1ec8c91d0762df90dad"
-$('#searchButton').on('click', fetchWeather());
-console.log("clicked");
+//this is a different way of implementing method+key. Learned from tutor. 
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         APIKey: "e3c409c704302318c1005301babd9b7c"
+//         }
+// }
+
+// $('#searchButton').on('click', fetchWeather());
+
+var APIKey = "e3c409c704302318c1005301babd9b7c"
+
 
 function fetchWeather() {
-    var cityText = document.getElementById('cityName');
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=';
-    var citySearch = $('city').val().trim();
+    var cityText = document.getElementById('input').value;
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+ cityText + '&appid=' + APIKey + '&units=imperial';
+    var citySearch = $('input').val();
     console.log(citySearch);
     
 
-localStorage.setItem("city", citySearch);
+localStorage.setItem("input", citySearch);
 
-fetch(requestUrl)
-.then(function (response) {
-    return response.json();
+fetch(requestUrl) 
+    .then( function (response) {
+        if (!response.ok) {
+    console.log("error")
+        } else {
+    return response.json()
+  }
 })
+
 .then(function (data) {
     console.log(data)
-    cityText.textContent = data.name;
+    cityName.textContent = data.name;
 
 
-    //this is a loop that can grab the min/max temps and write them into the weather panels 
+    //this is a loop that can grab the forecast elements and write them into the weather panels 
     for(i = 0; i<5; i++){
-        document.getElementById("min" + (i+1)).innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°";
-        document.getElementById()
+        document.getElementById("min" + (i+1)).innerHTML = "Min: " + Number(data.list[i].main.temp_min).toFixed(1)+ "°";
+        document.getElementById("wind" + (i+1)).innerHTML = "Wind: " + Number(data.list[i].wind.deg).toFixed(1)+ " " + "degrees";
+        document.getElementById("gust" + (i+1)).innerHTML = "Gust: " + Number(data.list[i].wind.gust).toFixed(1)+ " " + "gust" + "speed";
     }
 
     for(i = 0; i<5; i++){
-        document.getElementById("max" + (i+1)).innerHTML = "Max: " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
-        document.getElementById
+        document.getElementById("max" + (i+1)).innerHTML = "Max: " + Number(data.list[i].main.temp_max).toFixed(2) + "°";
+        document.getElementById("wind" + (i+1)).innerHTML = "Wind: " + Number(data.list[i].wind.deg).toFixed(1)+ " " + "degrees";
+        document.getElementById("gust" + (i+1)).innerHTML = "Gust: " + Number(data.list[i].wind.gust).toFixed(1)+ " " + "gust" + "speed";
     }
 })
 
 //this is a trick I learned from a friend. Can use .catch if the script fails above (i.e. no city name) to prompt user for correct parameters. 
 .catch(err => alert("Please enter a valid city name to receive weather data."))
 }
+
+
+var d = new Date();
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+
+//function to label days correctly
+function CheckDay(day){
+    if(day + d.getDay() > 6){
+        return day + d.getDay() - 7;
+    }
+    else{
+        return day + d.getDay();
+    }
+}
+
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
+    }
 
 //next task is to declare weather variables and API Key
 
