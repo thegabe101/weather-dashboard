2876ec2c5cc4f90d7145e814b42e077f2812ce92
Rayncor <sowa332@gmail.com>
2022-07-06 18:40:29 -0700

    changed html structure, simplified CSS, wrote loop for sketching temperature but don't know how to implement + having trouble with event listener

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//draw time header first 
+//draw time header element grabbers first 
 
 const timeEl = document.getElementById('time');
 const dateEl = document.getElementById('date');
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-
+//going to set an interval function to manage the clock and the date. not really sure but there must be a better way to do this. the problem here is that i dont need to check every second for a new date, but do want to do so re: time. 
 setInterval(() => {
     var time = new Date();
     var month = time.getMonth();
@@ -19,16 +19,17 @@ setInterval(() => {
     dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month];
 
 }, 1000);
-
+//this is my api key for open weather; not sure if this remains good forever. also not sure whether there is some reason to not make an API key a const but it seems like a const would be the right choice. 
 const APIKey = "2104664de283a1ec8c91d0762df90dad"
 
-function fetchWeather() {
 
+function fetchWeather() {
 
     var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=';
     var cityEl = document.getElementById("city");
     var cityName = document.getElementById("cityName");
     cityName.innerHTML = cityEl.value;
+    $('#searchButton').on('click', fetchWeather());
 
 fetch(requestUrl+cityEl.value+'&appid=2104664de283a1ec8c91d0762df90dad')
 .then(function (response) {
@@ -36,6 +37,20 @@ fetch(requestUrl+cityEl.value+'&appid=2104664de283a1ec8c91d0762df90dad')
 })
 .then(function (data) {
     console.log(data)
+    
+    //this is a loop that can grab the min/max temps and write them into the weather panels 
+    for(i = 0; i<5; i++){
+        document.getElementById("min" + (i+1)).innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°";
+    }
+
+    for(i = 0; i<5; i++){
+        document.getElementById("max" + (i+1)).innerHTML = "Max: " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
+    }
+})
+
+//this is a trick I learned from a friend. Can use .catch if the script fails above (i.e. no city name) to prompt user for correct parameters. 
+.catch(err => alert("Please enter a valid city name to receive weather data."))
+}
 
 //next task is to declare weather variables and API Key
 
